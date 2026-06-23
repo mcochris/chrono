a338d4ac1a73044524fb123ee5e2ad029cff309c
Chris Strawser <46349138+mcochris@users.noreply.github.com>
2026-06-23 04:23:12 -0400

    Added a index.html compressor to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 //
 // vite.config.ts
+//
 // Compression commented out because cloudflare automagically compresses files for us
 //
 import { defineConfig } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
+import { createHtmlPlugin } from 'vite-plugin-html'
 // import { compression, defineAlgorithm } from 'vite-plugin-compression2'
 // import zlib from 'node:zlib'
 
 export default defineConfig({
 	plugins: [
-		viteSingleFile()
+		viteSingleFile(),
+		createHtmlPlugin({ minify: true })
 		// 		compression({
 		// 			include: [/\.(js|mjs|css|json|svg|html)$/],
 		// 			threshold: 1024,
